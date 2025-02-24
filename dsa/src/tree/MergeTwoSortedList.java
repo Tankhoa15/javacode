9440a6a031f103d2baf38c5e3d0a14fcf95caa7c
Tankhoa15 <tankhoa1503kh@gmail.com>
2025-02-24 14:39:20 +0700

    "update code"

--- dsa/src/tree/MergeTwoSortedList.java
+++ dsa/src/tree/MergeTwoSortedList.java
@@ -3,6 +3,19 @@ package tree;
 public class MergeTwoSortedList {
 
     // way 1: recursive
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        if(list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
 //    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
 //        if(list1 == null || list2 == null){
 //            return (list1 != null) ? list1 : list2;
@@ -20,25 +33,25 @@ public class MergeTwoSortedList {
 //    }
 
     // way 2: iterative
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
-        ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
-
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                current.next = list1;
-                list1 = list1.next;
-            }else{
-                current.next = list2;
-                list2 = list2.next;
-            }
-            current = current.next;
-        }
-
-        current.next = (list1 != null) ? list1 : list2;
-
-        return dummy.next;
-    }
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+//        ListNode dummy = new ListNode(-1);
+//        ListNode current = dummy;
+//
+//        while(list1 != null && list2 != null){
+//            if(list1.val < list2.val){
+//                current.next = list1;
+//                list1 = list1.next;
+//            }else{
+//                current.next = list2;
+//                list2 = list2.next;
+//            }
+//            current = current.next;
+//        }
+//
+//        current.next = (list1 != null) ? list1 : list2;
+//
+//        return dummy.next;
+//    }
 
     public static void main(String[] args) {
         MergeTwoSortedList mergeTwoSortedList = new MergeTwoSortedList();
